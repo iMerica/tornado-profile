8c566933a3f2f2d30e6850b822d59ead282d4005
Michael <michaelmartinez@me.com>
2017-12-13 10:47:53 -0500

    Bump to Python3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     long_description=open("README.rst").read(),
     license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Topic :: Utilities",
